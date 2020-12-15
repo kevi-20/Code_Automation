71ab71d6e4f1a8484dbc5427c7ba2fee67f67e8c
873221 <73687993+873221@users.noreply.github.com>
2020-12-15 10:44:36 +0530

    Update UrbaanLader.java
    first testcase updated

--- Automation/src/main/java/PageObject/UrbaanLader.java
+++ Automation/src/main/java/PageObject/UrbaanLader.java
@@ -27,4 +27,58 @@ public class UrbaanLader {
 
 		this.driver = driver;
 	}
+	public void search(){
+		
+		driver.findElement(search).sendKeys("bookshelf");
+		try {
+
+			Thread.sleep(2000);
+
+			} catch (Exception e) {
+
+	
+
+			e.printStackTrace();
+
+			}
+		driver.findElement(enter).click();
+		log.info("searching the bookshelf");
+		}
+
+	public void selectPrice() {
+		driver.findElement(price).click();
+		WebElement upper=driver.findElement(By.xpath("//*[@id='filters-form']/div[1]/div/div/ul/li[2]/div[2]/div/div/ul/li[1]/div/div[2]/div[2]/div/div[2]/div"));
+       int uwidth=upper.getSize().width;            
+       Actions s=new Actions(driver);
+       s.dragAndDropBy(upper,(int) (-uwidth* (11.9)), 0);
+       s.build().perform();
+      log.info("Selecting the price range");
+       
+	}
+	public void print0(){
+		 try {
+
+				Thread.sleep(1000);
+
+				} catch (Exception e) {
+
+		
+
+				e.printStackTrace();
+
+				}
+		List<WebElement> shtitles = driver.findElements(shelvesTitle);
+		List<WebElement> shprices = driver.findElements(shelvesPrice);
+		int size=shtitles.size();
+		String[] name = new String[size];
+		String[] price = new String[size];
+		for (int i = 0; i <size; i++) {
+			name[i] = shtitles.get(i).getText();
+			price[i] = shprices.get(i).getText();
+			System.out.print(shtitles.get(i).getText() + "  ");
+			System.out.println(shprices.get(i).getText());
+			ExcelUtils.output0(name,price,size);	
+   }
+  
+}
 }
