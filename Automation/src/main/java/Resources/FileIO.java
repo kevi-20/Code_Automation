a2b30e112d332ec3d5379906c3fa028ea2c90b50
873221 <73687993+873221@users.noreply.github.com>
2020-12-15 10:46:07 +0530

    Update FileIO.java
    excel for 1st tc updated

--- Automation/src/main/java/Resources/FileIO.java
+++ Automation/src/main/java/Resources/FileIO.java
@@ -57,4 +57,25 @@ public class FileIO {
 			String location = prop.getProperty("firefox");
 			return location;
 		}
+	public static  void output0(String[]name ,String[]price,int size) {
+			workbook = new XSSFWorkbook();
+			sheet = workbook.createSheet("output0.xlsx");
+			for (int i = 0; i < size; i++) {
+				row = sheet.createRow(i);
+				row.createCell(0).setCellValue(name[i]);
+				row.createCell(1).setCellValue(price[i]);
+			}
+		
+			sheet.autoSizeColumn(0);
+			//Writing the output to Excel file using FileOutputStream
+			try {
+				fos = new FileOutputStream("output0.xlsx");
+				workbook.write(fos);
+				fos.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 }
