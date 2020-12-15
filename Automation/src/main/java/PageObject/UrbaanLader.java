e615bff333d086045cbfba1c3f65f402053be2aa
kevi-20 <72978175+kevi-20@users.noreply.github.com>
2020-12-15 12:01:59 +0530

    Update UrbaanLader.java
    code updated

--- Automation/src/main/java/PageObject/UrbaanLader.java
+++ Automation/src/main/java/PageObject/UrbaanLader.java
@@ -1,9 +1,12 @@
 package PageObject;
 
+import java.util.List;
 import java.util.Properties;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import Resources.BaseUI;
 import Resources.FileIO;
 
@@ -12,21 +15,42 @@ public class UrbaanLader {
 	public WebDriver driver;
 	static Properties prop;
 	static FileIO fileio;
+	By search=By.id("search");
+	By enter=By.xpath("//*[@id='search_button']/span");
+	By price=By.xpath("//*[@id='filters-form']/div[1]/div/div/ul/li[2]/div[1]");
+	By stock=By.xpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div/form/div[2]/div/input");
+    By storage=By.xpath("//*[@id='filters-form']/div[1]/div/div/ul/li[3]/div[1]");
+	By chairsearch=By.id("search");
+	By filter=By.id("filters_primary_category_Study_Chair");
+	By productTitle= By.xpath("//a[contains(text(),'Being At Home')]/parent::*/following-sibling::ul/li");
+    By chairTitle=By.className("product-title-sofa-mattresses");
+	By chairPrice=By.className("price-number");
+	By shelvesTitle=By.className("product-title-sofa-mattresses");
+    By shelvesPrice=By.className("price-number");
+   
 
 	public UrbaanLader() {
 		fileio = new FileIO();
 		prop = fileio.inputSetup();
 	}
 
-	public WebDriver createDriver(int option) {
+	
 
-		return driver = BaseUI.getDriver(option);
+ public UrbaanLader(WebDriver driver){
+	  this.driver=driver;
+	  
+ }
+	
+	public void createDriver(){
+		driver=BaseUI.getDriver();
+		
+	}
+	public void popuping() {
+		// TODO Auto-generated method stub
+		driver.findElement(By.linkText("Close")).click();
 	}
 
-	public UrbaanLader(WebDriver driver) {
-
-		this.driver = driver;
-	}
+	
 	public void search(){
 		
 		driver.findElement(search).sendKeys("bookshelf");
@@ -42,9 +66,9 @@ public class UrbaanLader {
 
 			}
 		driver.findElement(enter).click();
-		log.info("searching the bookshelf");
+		//log.info("searching the bookshelf");
 		}
-
+	
 	public void selectPrice() {
 		driver.findElement(price).click();
 		WebElement upper=driver.findElement(By.xpath("//*[@id='filters-form']/div[1]/div/div/ul/li[2]/div[2]/div/div/ul/li[1]/div/div[2]/div[2]/div/div[2]/div"));
@@ -52,7 +76,7 @@ public class UrbaanLader {
        Actions s=new Actions(driver);
        s.dragAndDropBy(upper,(int) (-uwidth* (11.9)), 0);
        s.build().perform();
-      log.info("Selecting the price range");
+     // log.info("Selecting the price range");
        
 	}
 	public void print0(){
@@ -77,8 +101,13 @@ public class UrbaanLader {
 			price[i] = shprices.get(i).getText();
 			System.out.print(shtitles.get(i).getText() + "  ");
 			System.out.println(shprices.get(i).getText());
-			ExcelUtils.output0(name,price,size);	
+		FileIO.output0(name,price,size);	
    }
   
 }
+
+
+
+	
+	
 }
