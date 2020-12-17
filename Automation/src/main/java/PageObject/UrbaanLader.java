b34ceab39fd1919bc0f7fb51e129de9537c13576
kavitha-Thangaraj <75778646+kavitha-Thangaraj@users.noreply.github.com>
2020-12-17 09:59:24 +0530

    Update UrbaanLader.java
    Updated test case 2

--- Automation/src/main/java/PageObject/UrbaanLader.java
+++ Automation/src/main/java/PageObject/UrbaanLader.java
@@ -1,149 +1,130 @@
 package PageObject;
 
+import java.util.List;
+
 import java.util.Properties;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 import Resources.BaseUI;
 import Resources.FileIO;
-
 
 public class UrbaanLader {
 	public WebDriver driver;
 	static Properties prop;
 	static FileIO fileio;
-	By search=By.id("search");
-	By enter=By.xpath("//*[@id='search_button']/span");
-	By price=By.xpath("//*[@id='filters-form']/div[1]/div/div/ul/li[2]/div[1]");
-	By stock=By.xpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div/form/div[2]/div/input");
-    By storage=By.xpath("//*[@id='filters-form']/div[1]/div/div/ul/li[3]/div[1]");
-	By chairsearch=By.id("search");
-	By filter=By.id("filters_primary_category_Study_Chair");
-	By productTitle= By.xpath("//a[contains(text(),'Being At Home')]/parent::*/following-sibling::ul/li");
-    By chairTitle=By.className("product-title-sofa-mattresses");
-	By chairPrice=By.className("price-number");
-	By shelvesTitle=By.className("product-title-sofa-mattresses");
-    By shelvesPrice=By.className("price-number");
+	static By Search;
+	static By ProductTitle;
+	static By Enter;
+	static By Price;
+	static By Stock;
+	static By Storage;
+	static By ShelvesTitle;
+	static By ShelvesPrice;
+	static By Collection;
+	By shelvesTitle = By.className("product-title-sofa-mattresses");
+	By shelvesPrice = By.className("price-number");
 
-	public UrbaanLader() {
+	public UrbaanLader(WebDriver driver) {
+		this.driver = driver;
 		fileio = new FileIO();
 		prop = fileio.inputSetup();
 	}
 
-	public WebDriver createDriver(int option) {
+	/*****************************************
+	 * Initialising the browser
+	 ******************************/
 
-		return driver = BaseUI.getDriver(option);
+	public void createDriver(int option) {
+		driver = BaseUI.getDriver(option);
+
 	}
 
-	public UrbaanLader(WebDriver driver) {
-
-		this.driver = driver;
-	}
 	public void popuping() {
-		//log.info("Closing the pop-up");
+
 		driver.findElement(By.linkText("Close")).click();
-		
+
 	}
-	public void navigate1(){
-		driver.navigate().to("https://www.urbanladder.com/");	
-		//log.info("Navigating to the home page");
+
+	// Searching the BookShelf
+	public void search() throws InterruptedException {
+		Search = By.id(prop.getProperty("search"));
+		driver.findElement(Search).sendKeys("bookshelf");
+		Thread.sleep(1000);
+		Enter = By.xpath(prop.getProperty("enter"));
+		driver.findElement(Enter).click();
+
 	}
-	
-public void search(){
-		
-		driver.findElement(search).sendKeys("bookshelf");
+
+	public void selectPrice() {
+		Price = By.xpath(prop.getProperty("price"));
+		driver.findElement(Price).click();
+		WebElement upper = driver.findElement(By.xpath(
+				"//*[@id='filters-form']/div[1]/div/div/ul/li[2]/div[2]/div/div/ul/li[1]/div/div[2]/div[2]/div/div[2]/div"));
+		int uwidth = upper.getSize().width;
+		Actions s = new Actions(driver);
+		s.dragAndDropBy(upper, (int) (-uwidth * (12.9)), 0);
+		s.build().perform();
+
+	}
+
+	public void storage() throws InterruptedException {
+		Storage = By.xpath(prop.getProperty("storage"));
+		driver.findElement(Storage).click();
+		Thread.sleep(1000);
+		WebElement check = driver.findElement(By.xpath(
+				"/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div/form/div[1]/div/div/ul/li[3]/div[2]/div/div/div/ul/li[1]/input"));
+		check.click();
+
+	}
+
+	public void stock() {
 		try {
 
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 
-			} catch (Exception e) {
-
-	
+		} catch (Exception e) {
 
 			e.printStackTrace();
 
-			}
-		driver.findElement(enter).click();
-		//log.info("searching the bookshelf");
 		}
+		Stock = By.xpath(prop.getProperty("stock"));
+		driver.findElement(Stock).click();
 
-	public void selectPrice() {
-		driver.findElement(price).click();
-		WebElement upper=driver.findElement(By.xpath("//*[@id='filters-form']/div[1]/div/div/ul/li[2]/div[2]/div/div/ul/li[1]/div/div[2]/div[2]/div/div[2]/div"));
-       int uwidth=upper.getSize().width;            
-       Actions s=new Actions(driver);
-       s.dragAndDropBy(upper,(int) (-uwidth* (12.2)), 0);
-       s.build().perform();
-      //log.info("Selecting the price range");
-       
-	}
- 
-	public void storage(){
-		
-		 driver.findElement(storage).click();
-		 try {
-
-				Thread.sleep(1000);
-
-				} catch (Exception e) {
-
-		
-
-				e.printStackTrace();
-
-				}
-		 WebElement check=driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div/form/div[1]/div/div/ul/li[3]/div[2]/div/div/div/ul/li[1]/input"));
-		 check.click();
-	   // log.info("Selecting the Storage Type");
-	   
-		
-		
+		// log.info("Selecting Stock type");
 	}
 
+	public void print() {
+		try {
 
-	public void stock()  {
-		 try {
+			Thread.sleep(1000);
 
-				Thread.sleep(1000);
+		} catch (Exception e) {
 
-				} catch (Exception e) {
+			e.printStackTrace();
 
-		
-
-				e.printStackTrace();
-
-				}
-		driver.findElement(stock).click();
-		//log.info("Selecting Stock type");
-} 
-
-	public void print(){
-		 try {
-
-				Thread.sleep(1000);
-
-				} catch (Exception e) {
-
-		
-
-				e.printStackTrace();
-
-				}
+		}
 		List<WebElement> shtitles = driver.findElements(shelvesTitle);
 		List<WebElement> shprices = driver.findElements(shelvesPrice);
-		
+
 		String[] name = new String[6];
 		String[] price = new String[6];
-		
+
 		for (int i = 0; i < 6; i++) {
 			name[i] = shtitles.get(i).getText();
 			price[i] = shprices.get(i).getText();
-			System.out.print(shtitles.get(i).getText() + "  ");
-			System.out.println(shprices.get(i).getText());
-			FileIO.output1(name,price);	
-    }
-   
-}
-  
-}
+			// System.out.print(shtitles.get(i).getText() + " ");
+			// System.out.println(shprices.get(i).getText());
+			FileIO.output1(name, price);
+		}
+
+	}
+
+	public void close() {
+		driver.quit();
+	}
+
 }
