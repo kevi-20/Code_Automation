5396cad6aa8168b76066579f4cc1fb8bfff99703
meg-ha <75722060+meg-ha@users.noreply.github.com>
2020-12-24 19:54:16 +0530

    Updated FileIO

--- Automation/src/main/java/Resources/FileIO.java
+++ Automation/src/main/java/Resources/FileIO.java
@@ -11,77 +11,93 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-
 public class FileIO {
 	// variables declaration
-		FileInputStream fis = null;
-		static FileOutputStream fos = null;
-		 static XSSFWorkbook workbook = null;
-		 static XSSFSheet sheet = null;
-		static XSSFRow row = null;
-		Properties prop = null;
+	FileInputStream fis = null;
+	static FileOutputStream fos = null;
+	static XSSFWorkbook workbook = null;
+	static XSSFSheet sheet = null;
+	static XSSFRow row = null;
+	Properties prop = null;
 
-		// Setting properties file for input
-		public Properties inputSetup() {
+	// Setting properties file for input
+	public Properties inputSetup() {
 
-			File file = new File(System.getProperty("user.dir") + "\\Resources\\config.properties");
-			System.out.println(file.getAbsolutePath());
+		File file = new File(System.getProperty("user.dir") + "\\Automation\\Resources\\Config.properties");
+		System.out.println(file.getAbsolutePath());
 
-			try {
-				fis = new FileInputStream(file);
-				prop = new Properties();
-				prop.load(fis);
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return prop;
+		try {
+			fis = new FileInputStream(file);
+			prop = new Properties();
+			prop.load(fis);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
-		public static  void output1(String[]name ,String[] price,int size) {
-			workbook = new XSSFWorkbook();
-			sheet = workbook.createSheet("output1");
-			for (int i = 0; i <size; i++) {
-				row = sheet.createRow(i);
-				row.createCell(0).setCellValue(name[i]);
-				row.createCell(1).setCellValue(price[i]);
-			}
-		
-			sheet.autoSizeColumn(0);
-			//Writing the output to Excel file using FileOutputStream
-			try {
-				fos = new FileOutputStream("output1.xlsx");
-				workbook.write(fos);
-				fos.close();
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		
+		return prop;
+	}
 
-		// to return the URL from properties file
-		public String getURL() {
-			inputSetup();
-			String URL = prop.getProperty("url");
-			return URL;
+	public static void output1(String[] name, String[] price, int size) {
+		workbook = new XSSFWorkbook();
+		sheet = workbook.createSheet("output1");
+		for (int i = 0; i < size; i++) {
+			row = sheet.createRow(i);
+			row.createCell(0).setCellValue(name[i]);
+			row.createCell(1).setCellValue(price[i]);
 		}
 
-		// to return the ChromeDriver location from properties file
-		public String getChromeDriverLocation() {
-			inputSetup();
-			String location = prop.getProperty("chrome");
-			return location;
+		sheet.autoSizeColumn(0);
+		// Writing the output to Excel file using FileOutputStream
+		try {
+			fos = new FileOutputStream("output1.xlsx");
+			workbook.write(fos);
+			fos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void chairDetails(String[] Name, String[] Price) {
+		workbook = new XSSFWorkbook();
+		sheet = workbook.createSheet("output2");
+		for (int i = 0; i < 4; i++) {
+			row = sheet.createRow(i);
+			row.createCell(0).setCellValue(Name[i]);
+			row.createCell(1).setCellValue(Price[i]);
+		}
+		try {
+			fos = new FileOutputStream("output2.xlsx");
+			workbook.write(fos);
+			fos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
-		// to return the GeckoDriver location from properties file
-		public String getGeckoDriverLocation() {
-			inputSetup();
-			String location = prop.getProperty("firefox");
-			return location;
-		}
+	}
+
+	// to return the URL from properties file
+	public String getURL() {
+		inputSetup();
+		String URL = prop.getProperty("url");
+		return URL;
+	}
+
+	// to return the ChromeDriver location from properties file
+	public String getChromeDriverLocation() {
+		inputSetup();
+		String location = prop.getProperty("chrome");
+		return location;
+	}
+
+	// to return the GeckoDriver location from properties file
+	public String getGeckoDriverLocation() {
+		inputSetup();
+		String location = prop.getProperty("firefox");
+		return location;
+	}
 }
-
