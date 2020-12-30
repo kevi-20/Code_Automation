6d944ba931933805922cc224179fa491afd0a645
meg-ha <75722060+meg-ha@users.noreply.github.com>
2020-12-30 18:14:38 +0530

    Update BaseUI.java

--- Automation/src/main/java/Resources/BaseUI.java
+++ Automation/src/main/java/Resources/BaseUI.java
@@ -1,8 +1,6 @@
 package Resources;
 
-
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
@@ -12,54 +10,47 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
- 
 
 public class BaseUI {
+	public static WebDriver driver;
+	static String URL;
+	public static Properties prop;
 
-		static String URL;
-		static String location;
-		static WebDriver driver;
-		public static Properties prop;
-		
-		public static WebDriver getDriver() throws IOException {
+	public static WebDriver getDriver() throws IOException {
+		FileIO fileio = new FileIO();
+		URL = fileio.getURL();
+		prop = new Properties();
+		FileInputStream fis = new FileInputStream(
+				System.getProperty("user.dir") + "\\Automation\\Resources\\Config.properties");
+		prop.load(fis);
+		URL = fileio.getURL();
+		String browserName = prop.getProperty("browser");
+		System.out.println(browserName);
+		if (browserName.equals("chrome")) {
+			String userdir = System.getProperty("user.dir");
+			System.setProperty("webdriver.chrome.driver", userdir + "\\Automation\\Drivers\\chromedriver.exe");
+			ChromeOptions chromeOptions = new ChromeOptions();
+			chromeOptions.addArguments("--disable-notifications");
+			driver = new ChromeDriver(chromeOptions);
 
-			FileIO fileio = new FileIO();
-			prop= new Properties();
-			FileInputStream fis=new FileInputStream(System.getProperty("user.dir") + "\\Automation\\Resources\\Config.properties");
+		} else if (browserName.equals("firefox")) {
 
-			 
+			String userdir = System.getProperty("user.dir");
+			System.setProperty("webdriver.gecko.driver", userdir + "\\Automation\\Drivers\\geckodriver.exe");
+			FirefoxOptions firefoxOptions = new FirefoxOptions();
+			firefoxOptions.addArguments("--disable-notifications");
+			driver = new FirefoxDriver(firefoxOptions);
 
-			prop.load(fis);
-			URL = fileio.getURL();
-			String browserName=prop.getProperty("browser");
-			if(browserName.equalsIgnoreCase("chrome"))
-			{
-			   location = fileio.getChromeDriverLocation();
-				System.setProperty("webdriver.chrome.driver", location);
-				ChromeOptions chromeOptions = new ChromeOptions();
-				chromeOptions.addArguments("--disable-notifications");
-	            driver = new ChromeDriver(chromeOptions);
-			}
-			else if (browserName.equalsIgnoreCase("firefox"))
-			{
-				location = fileio.getGeckoDriverLocation();
-				System.setProperty("webdriver.gecko.driver", location);
-
-				FirefoxOptions firefoxOptions = new FirefoxOptions();
-				firefoxOptions.addArguments("--disable-notifications");
-	            driver = new FirefoxDriver(firefoxOptions);
-			} else {
-				System.out.println("Not a valid driver");
-				System.exit(0);
-			}
-
-			driver.get(URL);
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-			return driver;
 		}
 
-		
-
+		else {
+			System.out.println("Not a valid option");
+			System.exit(0);
+		}
+		driver.get(URL);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		System.out.println(driver.getTitle());
+		return driver;
 	}
-
+}
