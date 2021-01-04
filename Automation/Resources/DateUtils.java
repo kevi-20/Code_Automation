2ac7aeaf04254346e1b682db528e5147fe2b7e65
873221 <73687993+873221@users.noreply.github.com>
2021-01-04 09:40:40 +0530

    Create DateUtils.java

---
+++ Automation/Resources/DateUtils.java
@@ -0,0 +1,10 @@
+package Resources;
+
+import java.util.Date;
+
+public class DateUtils {
+	public static String getTimeStamp(){
+		Date date = new Date();
+		return date.toString().replaceAll(":", "_").replaceAll(" ", "_");
+	}
+}
